c4085619a1bd3a0203317807f839a30c6da726d4
Endrey Márk <ungazik@gmail.com>
2022-03-06 18:34:26 +0100

    Add one more outmost loop for various distinct features in future

--- aux-scripts/unit-test-of-the-scripts.bash
+++ aux-scripts/unit-test-of-the-scripts.bash
@@ -2,25 +2,29 @@
 
 here=`dirname "$0"`;
 fixture=$here/unit-test-fixtures;
-mathjaxFilterer=$here/filter-mathjax-contents;
 
 declare -A run=([pl]="perl" [awk]="awk -f" [sed]="sed -n -f");
 
-echo $fixture/*.source.dat\
-|
-while read sourceFile;
+echo "Unit test";
+for feature in filter-mathjax-contents;
 	do
-		sampleName=${sourceFile%.source.dat};
-		expectationFile=$sampleName.expectation.dat;
-		echo "Samples:"
-		echo "- Sample \``basename $sampleName`':";
-		echo "  Testers:";
-		for extension in ${!run[@]};
+		echo "- Feature: $feature";
+		echo $fixture/$feature/*.source.dat\
+		|
+		while read sourceFile;
 			do
-				echo -en "\t- Tester \`${run[$extension]}': ";
-				if diff <(${run[$extension]} $mathjaxFilterer.$extension $sourceFile) $sampleName.expectation.dat;
-					then echo OK;
-					else echo Wrong;
-				fi;
+				sampleName=${sourceFile%.source.dat};
+				expectationFile=$sampleName.expectation.dat;
+				echo "  Samples:"
+				echo "  - Sample \``basename $sampleName`':";
+				echo "    Testers:";
+				for extension in ${!run[@]};
+					do
+						echo -en "    - Tester \`${run[$extension]}': ";
+						if diff <(${run[$extension]} $here/$feature.$extension $sourceFile) $sampleName.expectation.dat;
+							then echo OK;
+							else echo Wrong;
+						fi;
+				done;
 		done;
 done;
