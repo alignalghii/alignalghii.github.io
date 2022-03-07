62623e5a29aadc647566624c2c849e75cf564782
Endrey Márk <ungazik@gmail.com>
2022-03-07 12:42:28 +0100

    aux-scripts: unit test bash script: add comments

--- aux-scripts/unit-test-of-the-scripts.bash
+++ aux-scripts/unit-test-of-the-scripts.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 here=`dirname "$0"`;
+# it can differ from `pwd` if the script is called from elsewhere (just like now: called from `src/Makefile`)
+# Credit to: https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
+
 fixture=$here/unit-test-fixtures;
 
+# Implementation languages of the scripts to be tested:
 declare -A run=([pl]="perl" [awk]="awk -f" [sed]="sed -n -f");
 
 echo "Unit test";
@@ -11,7 +15,7 @@ for feature in filter-mathjax-contents skip-empty-lines-inside-mathjax;
 		echo "- Feature: $feature";
 		find $fixture/$feature -iname '*.source.dat'\
 		|
-		while read sourceFile;
+		while read sourceFile; # looped reading fom a list coming through pipeline
 			do
 				sampleName=${sourceFile%.source.dat};
 				echo "  Samples:"
@@ -19,6 +23,7 @@ for feature in filter-mathjax-contents skip-empty-lines-inside-mathjax;
 				echo "    Testers:";
 				for extension in ${!run[@]};
 					do
+						# The actual comparions:
 						echo -en "    - Tester \`${run[$extension]}': ";
 						if diff <(${run[$extension]} $here/$feature.$extension $sourceFile) $sampleName.expectation.dat;
 							then echo OK;
