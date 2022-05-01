82eb6111acc1654ecee3cb48fa2849f58e9af53c
Endrey Márk <ungazik@gmail.com>
2022-05-01 19:45:37 +0200

    aux-scripts: extend unit tester with `table-of-contents-generator-for-markdown`

--- aux-scripts/unit-test-of-the-scripts.bash
+++ aux-scripts/unit-test-of-the-scripts.bash
@@ -10,7 +10,7 @@ fixture=$here/unit-test-fixtures;
 declare -A run=([pl]="perl" [awk]="awk -f" [sed]="sed -n -f");
 
 echo "Unit test";
-for feature in table-of-contents-generator filter-mathjax-contents skip-empty-lines-inside-mathjax;
+for feature in table-of-contents-generator table-of-contents-generator-for-markdown filter-mathjax-contents skip-empty-lines-inside-mathjax;
 	do
 		echo "- Feature: $feature";
 		find $fixture/$feature -iname '*.source.dat'\
